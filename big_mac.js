99168819e316d8c3acbbafc79cf1403a4abf3ce9
Pela <140578861+pelaaaa@users.noreply.github.com>
2023-12-06 09:34:55 -0800

    Update big_mac.js

--- big_mac.js
+++ big_mac.js
@@ -12,95 +12,40 @@ var currentMarker = null;
 var voronoiLayer;
 
 
-function calculatePriceDifference(money1, money2) {
-    var price1 = parseFloat(money1);
-    var price2 = parseFloat(money2);
-    
-    if (!isNaN(price1) && !isNaN(price2)) {
-        var difference = price1 - price2;
-        return difference.toFixed(2); // Round to two decimal places
-    } else {
-        return "Invalid price data";
-    }
+//compare prices of two locations 
+function compareIt(firstLocationPrice, voronoiLayer) {
+    console.log("IN FUNCTION pricey price: " + firstLocationPrice);
+	voronoiLayer.on('click',function(e){
+		var polyClick = e.layer;
+		var polyId = polyClick.feature.properties.id;
+		var dolla = polyClick.feature.properties.big_mac;
+		console.log("second mclocation: "+dolla)
+		
+		alert("First location: "+firstLocationPrice+"<br>Second location: "+dolla)
+	})
 }
 
 
-function compareItBB(firstLocationPrice) {
-    console.log("IN FUNCTION pricey pricey price: " + firstLocationPrice);
-
-    // Perform the logic to retrieve data and handle the click event for the second location
-    fetch('https://raw.githubusercontent.com/pelaaaa/geog_371_lab_3/main/mcvoronoi.geojson')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            var voronoiLayer = L.geoJson(data, {
-                style: {
-                    fillColor: 'transparent',
-                    fillOpacity: 0.1,
-                    stroke: true,
-                    color: 'transparent'
-                }
-            }).addTo(map);
-
-            // Add an event listener to get the second location price
-            voronoiLayer.on('click', function (e) {
-                var clickedPolygon = e.layer;
-                var polygonId = clickedPolygon.feature.properties.id;
-                var money = clickedPolygon.feature.properties.big_mac;
-                console.log('big mac price in function: ' + money);
-
-                var matchedPointId = markerIds.find(function (id) {
-                    return id === polygonId;
-                });
-
-                if (matchedPointId) {
-                    fetch('https://raw.githubusercontent.com/pelaaaa/geog_371_lab_3/main/oregon.geojson')
-                        .then(function (response) {
-                            return response.json();
-                        })
-                        .then(function (pointsData) {
-                            var matchedPoint = pointsData.features.find(function (feature) {
-                                return feature.id === matchedPointId;
-                            });
-
-                            if (matchedPoint) {
-                                if (currentMarker) {
-                                    map.removeLayer(currentMarker);
-                                }
-
-                                // Once you have the second location details, calculate the price difference
-                                var secondLocationPrice = money; // Change this to the actual price of the second location
-                                var priceDifference = calculatePriceDifference(firstLocationPrice, secondLocationPrice);
-                                alert("Price Difference: " + priceDifference);
-                                
-                                // Remove the click event listener after comparison
-                                map.off('click', secondLocationHandler);
-                                map.closePopup();
-                            }
-                        });
-                }
-            });
-        });
-}
-
-
+//fetch point data
 fetch('https://raw.githubusercontent.com/pelaaaa/geog_371_lab_3/main/oregon.geojson')
     .then(function (response) {
         return response.json();
     })
     .then(function (json) {
+		//push point ids to a list that will be referenced later
         json.features.forEach(function (feature) {
             markerIds.push(feature.id);
         });
         console.log("markerIds: " + markerIds);
     });
 
+//fetch voronoi layer
 fetch('https://raw.githubusercontent.com/pelaaaa/geog_371_lab_3/main/mcvoronoi.geojson')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
+		//make voronoi transparent
         var voronoiLayer = L.geoJson(data, {
             style: {
                 fillColor: 'transparent',
@@ -110,16 +55,19 @@ fetch('https://raw.githubusercontent.com/pelaaaa/geog_371_lab_3/main/mcvoronoi.g
             }
         }).addTo(map);
 
+		//click voronoi to get id
         voronoiLayer.on('click', function (e) {
             var clickedPolygon = e.layer;
             var polygonId = clickedPolygon.feature.properties.id;
 			var money = clickedPolygon.feature.properties.big_mac;
 			console.log('big mac price: '+money);
-
+			
+			//match voronoi id with point id
             var matchedPointId = markerIds.find(function (id) {
                 return id === polygonId;
             });
 
+			//add matched point to map
             if (matchedPointId) {
                 fetch('https://raw.githubusercontent.com/pelaaaa/geog_371_lab_3/main/oregon.geojson')
                     .then(function (response) {
@@ -138,9 +86,10 @@ fetch('https://raw.githubusercontent.com/pelaaaa/geog_371_lab_3/main/mcvoronoi.g
                             currentMarker = L.geoJson(matchedPoint, {
                                 pointToLayer: function (feature, latlng) {
                                     var marker = L.marker(latlng);
+									//popup :)
                                     marker.bindPopup("I love me some Maccy D's!!!!!" + 
 									"<br>Big Mac price: " + money + "<br>" +
-									"<button onclick='compareItBB(\"" + money + "\")'>Compaaaaaare iiiit</button>"); 
+									"<button onclick='compareIt(\"" + money + "\", voronoiLayer)'>Compaaaaaare iiiit</button>"); 
                                     return marker;
                                 }
                             }).addTo(map);
